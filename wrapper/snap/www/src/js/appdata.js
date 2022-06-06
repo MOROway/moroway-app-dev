b0ad6fc84635c218bd626ab36d5263336a8ffd90
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2022-06-06 16:15:12 +0200

    update 20220606-161512

--- wrapper/snap/www/src/js/appdata.js
+++ wrapper/snap/www/src/js/appdata.js
@@ -3,12 +3,12 @@ const APP_DATA = {
     version: {
         major: 7,
         minor: 5,
-        patch: 1,
+        patch: 2,
         beta: 0,
         date: {
             year: 2022,
-            month: 5,
-            day: 30
+            month: 6,
+            day: 6
         }
     },
     platform: "snap",
