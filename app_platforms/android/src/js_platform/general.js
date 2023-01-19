2b2c1173f2ac79e487f1a04d19ea6bc051aa1333
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2023-01-19 19:27:57 +0100

    8.1.4

--- app_platforms/android/src/js_platform/general.js
+++ app_platforms/android/src/js_platform/general.js
@@ -15,8 +15,7 @@ function followLink(input1, input2, input3) {
             }
             input1 = input1.length == 0 ? "./index.html" : input1.substr(input1.length - 1, 1) == "/" ? input1 + "index.html" : input1.substr(input1.length - 5, 5) == ".html" ? input1 : input1 + "/index.html";
             if (queryString !== undefined) {
-                followLink = false;
-                WebJSInterface.handleQueryString(queryString.substr(1));
+                input1 += queryString;
             }
             if (hash !== undefined) {
                 input1 += hash;
