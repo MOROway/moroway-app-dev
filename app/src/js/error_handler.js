3549e59ab5f5d681278ecd60d21b70df80eefc36
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2023-10-11 16:48:31 +0200

    fix error handler

--- app/src/js/error_handler.js
+++ app/src/js/error_handler.js
@@ -5,7 +5,10 @@ window.addEventListener("error", function (event) {
         var actionElement = document.querySelector("#error-element-action");
         if (errorElement == null || actionElement == null) {
             document.body.innerHTML = '<div id="error-element"><div id="error-element-heading">FATAL ERROR</div><div id="error-element-action">Please report this error… Thanks!</div></div>';
-        } else {
+            errorElement = document.querySelector("#error-element");
+            actionElement = document.querySelector("#error-element-action");
+        }
+        if (errorElement != null && actionElement != null) {
             var messageElement = document.createElement("div");
             messageElement.textContent = event.message;
             messageElement.id = "error-element-message";
